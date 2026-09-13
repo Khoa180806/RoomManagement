0fe3cabb75e78880b1a48923577331e1967a3615
Khoa180806 <khoaphan180806@gmail.com>
2026-09-13 14:45:31 +0700

    chore: bootstrap Spring Boot API

---
+++ backend/src/main/java/com/khoa/roommanagement/RoomManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.khoa.roommanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomManagementApiApplication.class, args);
+	}
+
+}
